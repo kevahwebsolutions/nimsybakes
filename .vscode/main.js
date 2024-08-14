846b0c866fe8b7479e0db49e430ffda930501f0f
Kevah Web Developers <kevahwebsolutions@gmail.com>
2024-08-14 17:56:48 +0300

    innitial

---
+++ .vscode/main.js
@@ -0,0 +1,13 @@
+/// <reference path="c:/Users/mvick/.vscode/extensions/nur.script-0.2.1/@types/api.global.d.ts" />
+/// <reference path="c:/Users/mvick/.vscode/extensions/nur.script-0.2.1/@types/vscode.global.d.ts" />
+//  @ts-check
+//  API: https://code.visualstudio.com/api/references/vscode-api
+
+function activate(_context) {
+   window.showInformationMessage('Hello, World!');
+}
+
+function deactivate() {}
+
+module.exports = { activate, deactivate }
+<script src="https://kit.fontawesome.com/6b79018818.js" crossorigin="anonymous"></script>
